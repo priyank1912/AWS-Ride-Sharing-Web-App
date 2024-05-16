0aaa127bc0402ee0dad79eed1c50088ed0fd1902
priyank1912 <priyankshah2345@gmail.com>
2024-05-16 19:27:16 +0530

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_d2b1SBQCG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '32c52mpouff2i77lgurmkuhogq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://hxighjp39d.execute-api.us-west-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
